7f42848ee184064667e97be186efd07c09c3be11
НОУТ <Ebunyt@gmail.com>
2026-01-15 15:05:11 +0800

    create header and loader

---
+++ loader.js
@@ -0,0 +1,13 @@
+﻿function loadcomponent(id, file) {
+    fetch(file)
+        .then(response => {
+            if (response.ok) return response.text();
+            throw new Error("Could not load component " + file);
+        })
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(error => console.error(error));
+}
+
+loadcomponent('header-placeholder', 'header.html')
